ba654c3e8e9943ab2da86edc76368804350b0848
ckx0709 <178709230@qq.com>
2020-01-09 10:49:34 +0800

    add menu

---
+++ menu.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+#https://www.toutiao.com/i6759476744341684739/
+#目录
+function menu {
+	clear
+	echo
+	echo -e "\tSys Admin Menu\n"
+	echo -e "\shell1. Display disk space"
+	echo -e "\t2. Display logged on users"
+	echo -e "\s38. 使用函数循环写menu脚本"
+	echo -e "\t0. Exit program\n\n"
+	echo -en "\tEnter option:"
+	read -n 1 option
+}
+
+function diskspace {
+	clear 
+	df -k
+}
+
+function whoseon {
+	clear
+	who
+}
+
+function menusage {
+	clear
+	cat /proc/meminfo
+}
+#while [ 1 ] 等价于 while true
+while [ 1 ]
+do
+	menu
+	case $option in
+	0) 
+		break;;
+	1) 
+		diskspace;;
+	2)
+		whoseon;;
+	3)
+		menusage;;
+	*)
+		clear
+		echo "Sorry, wrong selection";;
+	esac
+	echo -en "\nHit any key to continue"
+	read -n 1 line
+done
+clear
